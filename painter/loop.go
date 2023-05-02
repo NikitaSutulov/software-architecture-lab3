31bbaae953638cc75713a868db69919a0c0e9639
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-05-02 14:27:24 +0300

    Fixed size in loop.go.

--- painter/loop.go
+++ painter/loop.go
@@ -24,7 +24,7 @@ type Loop struct {
 	stopReq bool
 }
 
-var size = image.Pt(400, 400)
+var size = image.Pt(800, 800)
 
 // Start запускає цикл подій. Цей метод потрібно запустити до того, як викликати на ньому будь-які інші методи.
 func (l *Loop) Start(s screen.Screen) {
