0d83f9d4c86924a65239f06a2596e3da95c49390
Nikita Sutulov <notsid212@gmail.com>
2023-05-02 14:23:30 +0300

    added operations
    Added opetations for creating background rectangle, cross figure, and move and reset operations.

--- painter/op.go
+++ painter/op.go
@@ -1,7 +1,10 @@
 package painter
 
 import (
+	"image"
 	"image/color"
+
+	"golang.org/x/image/draw"
 
 	"golang.org/x/exp/shiny/screen"
 )
@@ -45,4 +48,47 @@ func WhiteFill(t screen.Texture) {
 // GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
 func GreenFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
+}
+
+// BackgroundRectangle структура чорного прямокутника
+type BackgroundRectangle struct {
+	FirstPoint  image.Point
+	SecondPoint image.Point
+}
+
+// Do малює наш прямокутник
+
+func (op *BackgroundRectangle) Do(t screen.Texture) bool {
+	c := color.Black
+	t.Fill(image.Rect(op.FirstPoint.X, op.FirstPoint.Y, op.SecondPoint.X, op.SecondPoint.Y), c, screen.Src)
+	return false
+}
+
+type CrossFigure struct {
+	CentralPoint image.Point
+}
+
+func (op *CrossFigure) Do(t screen.Texture) bool {
+	c := color.RGBA{R: 255, G: 255, B: 0, A: 1}
+	t.Fill(image.Rect(op.CentralPoint.X-200, op.CentralPoint.Y+80, op.CentralPoint.X+200, op.CentralPoint.Y-80), c, draw.Src)
+	t.Fill(image.Rect(op.CentralPoint.X-80, op.CentralPoint.Y+200, op.CentralPoint.X+80, op.CentralPoint.Y-200), c, draw.Src)
+	return false
+}
+
+type MoveOperation struct {
+	X            int
+	Y            int
+	FiguresArray []*CrossFigure
+}
+
+func (op *MoveOperation) Do(t screen.Texture) bool {
+	for i := range op.FiguresArray {
+		op.FiguresArray[i].CentralPoint.X += op.X
+		op.FiguresArray[i].CentralPoint.Y += op.Y
+	}
+	return false
+}
+
+func Reset(t screen.Texture) {
+	t.Fill(t.Bounds(), color.Black, screen.Src)
 }
