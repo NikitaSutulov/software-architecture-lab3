152de744820f358826f71b5281f38e5aec778d06
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-11 22:13:46 +0300

    Updated uistate.go and parser.go
    Moved some logic with operations to uistate.go.

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -13,18 +13,11 @@ import (
 
 // Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
 type Parser struct {
-	uistate         Uistate
-	moveOperations  []painter.Operation
-	updateOperation painter.Operation
+	uistate Uistate
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
-	if p.uistate.backgroundColor == nil {
-		p.uistate.backgroundColor = painter.OperationFunc(painter.Reset)
-	}
-	if p.updateOperation != nil {
-		p.updateOperation = nil
-	}
+	p.uistate.ResetOperations()
 
 	scanner := bufio.NewScanner(in)
 	scanner.Split(bufio.ScanLines)
@@ -38,36 +31,9 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 		}
 	}
 
-	var res []painter.Operation
-
-	if p.uistate.backgroundColor != nil {
-		res = append(res, p.uistate.backgroundColor)
-	}
-	if p.uistate.backgroundRectangle != nil {
-		res = append(res, p.uistate.backgroundRectangle)
-	}
-	if len(p.moveOperations) != 0 {
-		res = append(res, p.moveOperations...)
-		p.moveOperations = nil
-	}
-	if len(p.uistate.figuresArray) != 0 {
-		for _, figure := range p.uistate.figuresArray {
-			res = append(res, figure)
-		}
-	}
-	if p.updateOperation != nil {
-		res = append(res, p.updateOperation)
-	}
+	res := p.uistate.GetOperations()
 
 	return res, nil
-}
-
-func (p *Parser) reset() {
-	p.uistate.backgroundColor = nil
-	p.uistate.backgroundRectangle = nil
-	p.uistate.figuresArray = nil
-	p.moveOperations = nil
-	p.updateOperation = nil
 }
 
 func (p *Parser) parse(cmdl string) error {
@@ -79,49 +45,41 @@ func (p *Parser) parse(cmdl string) error {
 		if len(words) != 1 {
 			return fmt.Errorf("wrong number of arguments for white command")
 		}
-		p.uistate.backgroundColor = painter.OperationFunc(painter.WhiteFill)
+		p.uistate.WhiteBackground()
 	case "green":
 		if len(words) != 1 {
 			return fmt.Errorf("wrong number of arguments for green command")
 		}
-		p.uistate.backgroundColor = painter.OperationFunc(painter.GreenFill)
+		p.uistate.GreenBackground()
 	case "bgrect":
 		parameters, err := checkForErrorsInParameters(words, 5)
 		if err != nil {
 			return err
 		}
-		p.uistate.backgroundRectangle = &painter.BackgroundRectangle{
-			FirstPoint:  image.Point{X: parameters[0], Y: parameters[1]},
-			SecondPoint: image.Point{X: parameters[2], Y: parameters[3]},
-		}
+		p.uistate.BackgroundRectangle(image.Point{X: parameters[0], Y: parameters[1]}, image.Point{X: parameters[2], Y: parameters[3]})
 	case "figure":
 		parameters, err := checkForErrorsInParameters(words, 3)
 		if err != nil {
 			return err
 		}
 
-		figure := painter.CrossFigure{
-			CentralPoint: image.Point{X: parameters[0], Y: parameters[1]},
-		}
-		p.uistate.figuresArray = append(p.uistate.figuresArray, &figure)
+		p.uistate.AddFigure(image.Point{X: parameters[0], Y: parameters[1]})
 	case "move":
 		parameters, err := checkForErrorsInParameters(words, 3)
 		if err != nil {
 			return err
 		}
-		moveOp := painter.MoveOperation{X: parameters[0], Y: parameters[1], FiguresArray: p.uistate.figuresArray}
-		p.moveOperations = append(p.moveOperations, &moveOp)
+		p.uistate.AddMoveOperation(parameters[0], parameters[1])
 	case "reset":
 		if len(words) != 1 {
 			return fmt.Errorf("wrong number of arguments for reset command")
 		}
-		p.reset()
-		p.uistate.backgroundColor = painter.OperationFunc(painter.Reset)
+		p.uistate.ResetStateAndBackground()
 	case "update":
 		if len(words) != 1 {
 			return fmt.Errorf("wrong number of arguments for update command")
 		}
-		p.updateOperation = painter.UpdateOp
+		p.uistate.SetUpdateOperation()
 	default:
 		return fmt.Errorf("invalid command %v", words[0])
 	}
--- painter/lang/uistate.go
+++ painter/lang/uistate.go
@@ -2,10 +2,91 @@ package lang
 
 import (
 	"github.com/NikitaSutulov/software-architecture-lab3/painter"
+	"image"
 )
 
 type Uistate struct {
 	backgroundColor     painter.Operation
 	backgroundRectangle *painter.BackgroundRectangle
 	figuresArray        []*painter.CrossFigure
+	moveOperations      []painter.Operation
+	updateOperation     painter.Operation
+}
+
+func (u *Uistate) Reset() {
+	u.backgroundColor = nil
+	u.backgroundRectangle = nil
+	u.figuresArray = nil
+	u.moveOperations = nil
+	u.updateOperation = nil
+}
+
+func (u *Uistate) GetOperations() []painter.Operation {
+	var ops []painter.Operation
+
+	if u.backgroundColor != nil {
+		ops = append(ops, u.backgroundColor)
+	}
+	if u.backgroundRectangle != nil {
+		ops = append(ops, u.backgroundRectangle)
+	}
+	if len(u.moveOperations) != 0 {
+		ops = append(ops, u.moveOperations...)
+		u.moveOperations = nil
+	}
+	if len(u.figuresArray) != 0 {
+		for _, figure := range u.figuresArray {
+			ops = append(ops, figure)
+		}
+	}
+	if u.updateOperation != nil {
+		ops = append(ops, u.updateOperation)
+	}
+
+	return ops
+}
+
+func (u *Uistate) ResetOperations() {
+	if u.backgroundColor == nil {
+		u.backgroundColor = painter.OperationFunc(painter.Reset)
+	}
+	if u.updateOperation != nil {
+		u.updateOperation = nil
+	}
+}
+
+func (u *Uistate) GreenBackground() {
+	u.backgroundColor = painter.OperationFunc(painter.GreenFill)
+}
+
+func (u *Uistate) WhiteBackground() {
+	u.backgroundColor = painter.OperationFunc(painter.GreenFill)
+}
+
+func (u *Uistate) BackgroundRectangle(firstPoint image.Point, secondPoint image.Point) {
+	u.backgroundRectangle = &painter.BackgroundRectangle{
+		FirstPoint:  firstPoint,
+		SecondPoint: secondPoint,
+	}
+}
+
+func (u *Uistate) AddFigure(centralPoint image.Point) {
+	figure := painter.CrossFigure{
+		CentralPoint: centralPoint,
+	}
+	u.figuresArray = append(u.figuresArray, &figure)
+}
+
+func (u *Uistate) AddMoveOperation(x int, y int) {
+	moveOp := painter.MoveOperation{X: x, Y: y, FiguresArray: u.figuresArray}
+	u.moveOperations = append(u.moveOperations, &moveOp)
+}
+
+func (u *Uistate) ResetStateAndBackground() {
+	u.Reset()
+	u.backgroundColor = painter.OperationFunc(painter.Reset)
+}
+
+func (u *Uistate) SetUpdateOperation() {
+	u.updateOperation = painter.UpdateOp
 }
